1ff55eaf04516cc00c2bde22de70ff722d705c76
Shrouk Yasser <101811358+SHYA95@users.noreply.github.com>
2022-05-13 17:36:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LoginUi from "./LoginUi";
+
+ReactDOM.render(<LoginUi />, document.getElementById("root"));
---
+++ src/LoginUi/LoginUi.js
@@ -0,0 +1,45 @@
+import "./LoginUi.css";
+import profile from "./../image/b.png";
+import email from "./../image/email.jpg";
+import pass from "./../image/pass.png";
+function LoginUi() {
+  return (
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input type="text" placeholder="user e-mail" className="name" />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email" />
+              <input type="password" placeholder="password" className="name" />
+            </div>
+            <div className="login-button">
+              <a href="-blank">
+                <button>Log in</button>
+              </a>
+            </div>
+
+            <p className="link">
+              <a href="#" onclick="#">
+                Forget Password?
+              </a>
+              <a href="#" onclick="#">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default LoginUi;
